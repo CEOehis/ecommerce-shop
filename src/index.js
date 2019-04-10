d078086962ca8fa89ef4e1b3255c184ec8177512
Celestine Ekoh-Ordan <CEO.ehis@outlook.com>
2019-04-10 15:34:55 +0100

    add logging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import express from 'express';
+import expressWinston from 'express-winston';
+import winston from 'winston';
+import morgan from 'morgan';
+import log from 'fancy-log';
 
 const app = express();
+app.use(morgan('dev'));
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+  })
+);
 
 app.get('/', (req, res) => {
   return res.status(200).json({
@@ -8,9 +22,20 @@ app.get('/', (req, res) => {
     message: 'ecommerce shop api',
   });
 });
+
+app.use(
+  expressWinston.errorLogger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+  })
+);
+
 const port =
   parseInt(process.env.NODE_ENV === 'test' ? 8378 : process.env.PORT, 10) ||
   5000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port} 🚀🚀🚀`);
+  log(`Server is running on http://localhost:${port} 🚀🚀🚀`);
 });
